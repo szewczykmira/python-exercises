23c171fecff33094ddcbdfb2ec5329f391575b79
Mira <szewczyk.mira@gmail.com>
2017-10-18 19:01:10 +0200

    Uniterative groupby

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 from functools import reduce
+import itertools
 
 # Sum of digits can be found in Basic Syntax/1.py
 
@@ -9,3 +10,13 @@ def multiplication(*args):
 
 def longer_than(min_len, *strings):
     return [string for string in strings if len(string) > min_len]
+
+
+def groupby(fun, *elems):
+    result = {}
+    for key, value in itertools.groupby(elems, fun):
+        value = list(value)
+        current_val = result.get(key, [])
+        current_val.extend(value)
+        result.update({key: current_val})
+    return result
